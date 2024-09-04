a2b0c1657c2bf8b6c9cc763c7e623ab52faf6873
Eduardo Roth <eduardo@eduardoroth.dev>
2024-09-04 09:47:07 -0600

    chore(): catch exception of bootstrapApplication

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,4 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig);
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.log(err));
